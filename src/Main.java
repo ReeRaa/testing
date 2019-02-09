d7fef137dd8a874ce2f340060abc559c00e20486
RRaa <35782666+RRaa@users.noreply.github.com>
2019-02-09 15:48:22 +0200

    Puuviljad-jurviljad

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Puuviljad");
-        System.out.println("Juurviljad");
+        System.out.println("Juurikad");
     }
 }
